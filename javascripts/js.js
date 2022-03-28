4e832849a73169e66813e7d0141a2d6af20cdbbf
inchusya <imkosulnikova@edu.hse.ru>
2022-03-28 23:11:02 +0300

    23:10

--- javascripts/js.js
+++ javascripts/js.js
@@ -1,3 +1,18 @@
+$(document).ready(function(){
+setTimeout(function(){
+  $(".alarm").css("display","block");
+},60000);  
+});
+
+$(function () {
+let count = 0;
+$(".one").click(function () {
+count += 1;
+if (count == 2) {
+$(".two").removeClass("two");
+}
+});
+});
 
 
 $(function(){
@@ -146,34 +161,21 @@ $(document).ready(function() {
     $("#callone").append(rez);
     setTimeout( function() { rez.remove(); }, 500);
 
-    var str1 = (str[str.length - 3] + str[str.length - 2] + str[str.length - 1]).toString()
-    if (str1.includes("1") && str1.includes("2") && str1.includes("3")) {
-      alert("Код исправен")
-      $(".s1").toggleClass("white")
-    }
+
   });
 });
+
+
+
+
+
+
 $(document).ready(function() {
 	$(".two").click( function () {
     str += "2"
     let rez1 = $('<div class="popup1">2 </div>');
-    $("#calltwo").append(rez1);
+    $("#callthree").append(rez1);
     setTimeout( function() { rez1.remove(); }, 500);
-
-    var str1 = (str[str.length - 3] + str[str.length - 2] + str[str.length - 1]).toString()
-    if (str1.includes("1") && str1.includes("2") && str1.includes("3")) {
-      alert("Код исправен")
-      $("p, body, .screenimg, .voz, .speedimg, .dropbar, .bar, .GSNASA11").css("animation-play-state", "running")
-      $(".weather, .phone, .map, .course, .screen, .buttons").css("animation-play-state", "running")
-      $(".one, .two, .three, .four, .five, .six, .seven, .eight, .nine, .net, .zero, .star").css("animation-play-state", "running")
-      $(".p125, .a5674c7, .a6f7ddc, .anb617, .a324789").css("animation-play-state", "running")
-      $(".circlebuttons, .square, .verticalbar, .inbar, .lever").css("animation-play-state", "running")
-      $(".LOC19, .loc18, .loc17, .loc16, .loc15, .loc14, .loc13").css("animation-play-state", "running")
-      $("#number21, .number22, .number22, .number23, .number25, .number26, .number27, .number28, .number29").css("animation-play-state", "running")
-      setTimeout(function() {
-             $(".screenimg, .speedimg").css("animation-play-state", "paused");
-           }, 9900);
-    }
   });
 });
 
@@ -185,11 +187,19 @@ $(document).ready(function() {
     setTimeout( function() { rez2.remove(); }, 500);
 
     var str1 = (str[str.length - 3] + str[str.length - 2] + str[str.length - 1]).toString()
-    if (str1.includes("1") && str1.includes("2") && str1.includes("3")) {
+    if (str1.includes("1") && str1.includes("4") && str1.includes("3")) {
       alert("Код исправен")
-      $(".s1").toggleClass("white")
+      $("p, body, .screenimg, .voz, .speedimg, .dropbar, .bar, .GSNASA11, .dropbar, .dropbar2, .dropbar3").css("animation-play-state", "running")
+      $(".weather, .phone, .map, .course, .screen, .compasimg, .buttons").css("animation-play-state", "running")
+      $(".one, .two, .three, .four, .five, .six, .seven, .eight, .nine, .net, .zero, .star").css("animation-play-state", "running")
+      $(".p125, .a5674c7, .a6f7ddc, .anb617, .a324789").css("animation-play-state", "running")
+      $(".circlebuttons, .square, .verticalbar, .inbar, .lever, .spin1img, .spin2img").css("animation-play-state", "running")
+      $(".LOC19, .loc18, .loc17, .loc16, .loc15, .loc14, .loc13").css("animation-play-state", "running")
+      $("#number21, .number22, .number22, .number23, .number25, .number26, .number27, .number28, .number29").css("animation-play-state", "running")
+      setTimeout(function() {
+             $(".screenimg, .speedimg").css("animation-play-state", "paused");
+           }, 9900);
     }
-
   });
 });
 
@@ -199,6 +209,7 @@ $(document).ready(function() {
     let rez3 = $('<div class="popup3">4 </div>');
     $("#callfour").append(rez3);
     setTimeout( function() { rez3.remove(); }, 500);
+
   });
 });
 
@@ -244,8 +255,47 @@ $(document).ready(function() {
     let rez8 = $('<div class="popup8">9 </div>');
     $("#callnine").append(rez8);
     setTimeout( function() { rez8.remove(); }, 500);
+    var str1 = (str[str.length - 3] + str[str.length - 2] + str[str.length - 1]).toString()
+    if (str1.includes("1") && str1.includes("100") && str1.includes("9")) {
+      alert("Код исправен")
+      $("p, body, .screenimg, .voz, .speedimg, .dropbar, .bar, .GSNASA11, .dropbar, .dropbar2, .dropbar3").css("animation-play-state", "running")
+      $(".weather, .phone, .map, .course, .screen, .compasimg, .buttons").css("animation-play-state", "running")
+      $(".one, .two, .three, .four, .five, .six, .seven, .eight, .nine, .net, .zero, .star").css("animation-play-state", "running")
+      $(".p125, .a5674c7, .a6f7ddc, .anb617, .a324789").css("animation-play-state", "running")
+      $(".circlebuttons, .square, .verticalbar, .inbar, .lever, .spin1img, .spin2img").css("animation-play-state", "running")
+      $(".LOC19, .loc18, .loc17, .loc16, .loc15, .loc14, .loc13").css("animation-play-state", "running")
+      $("#number21, .number22, .number22, .number23, .number25, .number26, .number27, .number28, .number29").css("animation-play-state", "running")
+      setTimeout(function() {
+             $(".screenimg, .speedimg").css("animation-play-state", "paused");
+           }, 9900);
+    }
   });
 });
+
+
+// $(document).ready(function() {
+// 	$(".one").click( function () {
+//     str += "1"
+//     let rez = $('<div class="popup">1 </div>');
+//     $("#callone").append(rez);
+//     setTimeout( function() { rez.remove(); }, 500);
+//
+//     var str1 = (str[str.length - 3] + str[str.length - 2] + str[str.length - 1]).toString()
+//     if (str1.includes("1") && str1.includes("1") && str1.includes("9")) {
+//       alert("Код исправен")
+//       $("p, body, .screenimg, .voz, .speedimg, .dropbar, .bar, .GSNASA11, .dropbar, .dropbar2, .dropbar3").css("animation-play-state", "running")
+//       $(".weather, .phone, .map, .course, .screen, .compasimg, .buttons").css("animation-play-state", "running")
+//       $(".one, .two, .three, .four, .five, .six, .seven, .eight, .nine, .net, .zero, .star").css("animation-play-state", "running")
+//       $(".p125, .a5674c7, .a6f7ddc, .anb617, .a324789").css("animation-play-state", "running")
+//       $(".circlebuttons, .square, .verticalbar, .inbar, .lever, .spin1img, .spin2img").css("animation-play-state", "running")
+//       $(".LOC19, .loc18, .loc17, .loc16, .loc15, .loc14, .loc13").css("animation-play-state", "running")
+//       $("#number21, .number22, .number22, .number23, .number25, .number26, .number27, .number28, .number29").css("animation-play-state", "running")
+//       setTimeout(function() {
+//              $(".screenimg, .speedimg").css("animation-play-state", "paused");
+//            }, 9900);
+//     }
+//   });
+// });
 
 $(document).ready(function() {
 	$(".net").click( function () {
